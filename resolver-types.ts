eec214718cf2f4a602b936baa4ddb87cfac8d89b
Cole Potrocky <cole@potrocky.com>
2019-04-22 10:50:02 -0400

    union mapper repro

---
+++ resolver-types.ts
@@ -0,0 +1,145 @@
+export type Maybe<T> = T | null;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Book = {
+  id: Scalars["ID"];
+  title: Scalars["String"];
+  author: Scalars["String"];
+  pages?: Maybe<Scalars["Int"]>;
+};
+
+export type Movie = {
+  id: Scalars["ID"];
+  title: Scalars["String"];
+  length?: Maybe<Scalars["Int"]>;
+};
+
+export type MovieLike = Movie | Book;
+import { MovieEntity } from "./types";
+
+import { GraphQLResolveInfo } from "graphql";
+
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+export type ResolverFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Promise<TResult> | TResult;
+
+export type StitchingResolver<TResult, TParent, TContext, TArgs> = {
+  fragment: string;
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
+  | ResolverFn<TResult, TParent, TContext, TArgs>
+  | StitchingResolver<TResult, TParent, TContext, TArgs>;
+
+export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => AsyncIterator<TResult> | Promise<AsyncIterator<TResult>>;
+
+export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, TParent, TContext, TArgs>;
+}
+
+export type SubscriptionResolver<
+  TResult,
+  TParent = {},
+  TContext = {},
+  TArgs = {}
+> =
+  | ((
+      ...args: any[]
+    ) => SubscriptionResolverObject<TResult, TParent, TContext, TArgs>)
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+
+export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Maybe<TTypes>;
+
+export type NextResolverFn<T> = () => Promise<T>;
+
+export type DirectiveResolverFn<
+  TResult = {},
+  TParent = {},
+  TContext = {},
+  TArgs = {}
+> = (
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+/** Mapping between all available schema types and the resolvers types */
+export type ResolversTypes = {
+  String: Scalars["String"];
+  Boolean: Scalars["Boolean"];
+  Movie: MovieEntity;
+  ID: Scalars["ID"];
+  Int: Scalars["Int"];
+  Book: Book;
+  MovieLike: MovieLike;
+};
+
+export type BookResolvers<
+  ContextType = any,
+  ParentType = ResolversTypes["Book"]
+> = {
+  id?: Resolver<ResolversTypes["ID"], ParentType, ContextType>;
+  title?: Resolver<ResolversTypes["String"], ParentType, ContextType>;
+  author?: Resolver<ResolversTypes["String"], ParentType, ContextType>;
+  pages?: Resolver<Maybe<ResolversTypes["Int"]>, ParentType, ContextType>;
+};
+
+export type MovieResolvers<
+  ContextType = any,
+  ParentType = ResolversTypes["Movie"]
+> = {
+  id?: Resolver<ResolversTypes["ID"], ParentType, ContextType>;
+  title?: Resolver<ResolversTypes["String"], ParentType, ContextType>;
+  length?: Resolver<Maybe<ResolversTypes["Int"]>, ParentType, ContextType>;
+};
+
+export type MovieLikeResolvers<
+  ContextType = any,
+  ParentType = ResolversTypes["MovieLike"]
+> = {
+  __resolveType: TypeResolveFn<"Movie" | "Book", ParentType, ContextType>;
+};
+
+export type Resolvers<ContextType = any> = {
+  Book?: BookResolvers<ContextType>;
+  Movie?: MovieResolvers<ContextType>;
+  MovieLike?: MovieLikeResolvers;
+};
+
+/**
+ * @deprecated
+ * Use "Resolvers" root object instead. If you wish to get "IResolvers", add "typesPrefix: I" to your config.
+ */
+export type IResolvers<ContextType = any> = Resolvers<ContextType>;
---
+++ types.ts
@@ -0,0 +1,4 @@
+type MovieEntity = {
+  fullName: string;
+  lengthInHours: number;
+};
